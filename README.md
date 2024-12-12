a676ed8fb522651664b78624e0546e1cc8b46b4b
Biker_Tax <149541420+John-ODell@users.noreply.github.com>
2024-12-11 21:14:33 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Password Generator by John O'Dell
+
+In this project, we use a HTTP local host to input the length, title name, and type of password we want. It is then saved to a local CSV file that can be read at later for use of the password. 
+The password can either contain letters and numbers or include symbols as well.
+The length is determined by the user on the HTTP page, along with the name. 
+
+There is a choice between the symbols as some websites have different allowences, as well with the length ranging between 12-20 characters form what I have found.
+
+Read the Jupyter Examples for further details and explination on how the sketch is working and to read passwords after they have been generated.
---
+++ password_generator.py
@@ -0,0 +1,90 @@
+from flask import Flask, request
+import pandas as pd
+import random
+import string
+
+# Define the symbols dictionary
+symbols = {
+    "ampersand": "&",
+    "asterisk": "*",
+    "at": "@",
+    "caret": "^",
+    "comma": ",",
+    "curly_brace_close": "}",
+    "curly_brace_open": "{",
+    "dollar": "$",
+    "exclamation": "!",
+    "hash": "#",
+    "parenthesis_close": ")",
+    "parenthesis_open": "(",
+    "percent": "%",
+    "period": ".",
+    "plus": "+",
+    "question_mark": "?",
+    "tilde": "~",
+    "underscore": "_"
+}
+
+
+# Password generator functions
+def gen_password_letters_numbers(length):
+    letters = string.ascii_letters
+    numbers = string.digits
+    all_characters = list(letters + numbers)  # Convert to list
+    random.shuffle(all_characters)
+    password = ''.join(random.choice(all_characters) for i in range(length))
+    return password
+
+
+def gen_password_symbols(length):
+    letters = string.ascii_letters
+    numbers = string.digits
+    all_characters = list(letters + numbers + ''.join(symbols.values()))  # Convert to list
+    random.shuffle(all_characters)
+    password = ''.join(random.choice(all_characters) for i in range(length))
+    return password
+
+
+# Initialize an empty DataFrame
+password_list = pd.DataFrame(columns=['Name', 'Password'])
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global password_list
+    if request.method == 'POST':
+        name = request.form['name']
+        length = int(request.form['length'])
+        use_symbols = request.form.get('use_symbols')
+
+        if use_symbols:
+            password = gen_password_symbols(length)
+        else:
+            password = gen_password_letters_numbers(length)
+
+        # Add the new password to the DataFrame
+        new_row = pd.DataFrame({'Name': [name], 'Password': [password]})
+        password_list = pd.concat([password_list, new_row], ignore_index=True)
+
+        # Debugging: Print the DataFrame to the console
+        print(password_list)
+
+        # Save the DataFrame to a CSV file
+        password_list.to_csv('passwords.csv', index=False)
+
+        return f"Password for {name}: {password} (Saved to CSV)"
+
+    return '''
+        <form method="post">
+            Name: <input type="text" name="name"><br>
+            Length: <input type="number" name="length"><br>
+            Use Symbols: <input type="checkbox" name="use_symbols"><br>
+            <input type="submit" value="Generate and Save">
+        </form>
+    '''
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
